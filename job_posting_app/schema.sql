4085f78d03c66534fde8071519b9bf3b0c6443f8
kymalcansadoape <kymalcansadoape@gmail.com>
2024-09-04 19:46:17 +0800

    initial commit

---
+++ job_posting_app/schema.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS applicant;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS jobs;
+DROP TABLE IF EXISTS applications;
+
+
+CREATE TABLE users(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE company(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    company_name TEXT,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE applications(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    applicant_id INTEGER,
+    job_id INTEGER,
+    FOREIGN KEY(applicant_id) REFERENCES applicant(id),
+    FOREIGN KEY(job_id) REFERENCES jobs(id)
+);
+
+CREATE TABLE jobs(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    job_name TEXT,
+    job_description TEXT,
+    job_level TEXT,
+    company_id INTEGER,
+    FOREIGN KEY(company_id) REFERENCES company(id)
+);
+
+CREATE TABLE applicant(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    user_id INTEGER,
+    resume_path TEXT,
+    years_of_experience INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+
+
